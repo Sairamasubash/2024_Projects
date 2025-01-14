57d5a3992b619a356ae01303a826cf4d6591f8db
Sairamasubash <112271055+Sairamasubash@users.noreply.github.com>
2025-01-14 16:49:35 -0600

    Delete my-app directory

--- my-app/src/app/pipe/searchlist.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SearchlistPipe } from './searchlist.pipe';
-
-describe('SearchlistPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SearchlistPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
